14d850052139875a484444970b8027fc5cb2faad
vijay-rajput-777 <virajrajput7861ag@gmail.com>
2026-09-25 13:46:12 +0530

    Q7

--- 7 - Alternates in an Array/Q7.js
+++ 7 - Alternates in an Array/Q7.js
@@ -11,6 +11,7 @@
 // Skip fourth element: 4
 
 //Question link : (https://www.geeksforgeeks.org/problems/print-alternate-elements-of-an-array/1?page=1&category=Arrays&difficulty=Basic&sortBy=submissions)
+
  function getAlternates(arr) {
     
         let result =[]
